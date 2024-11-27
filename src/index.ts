c2e437d880555b252e75f44009d80799ad8b5b34
Diyan Yagari Pratama <diyan.pratama@autopedia.id>
2024-11-28 01:03:34 +0700

    fix: try add to allow ip4 and ip6

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use(errorHandler);
 
 // Start the server after data source initialization
 AppDataSource.initialize().then(() => {
-  app.listen(PORT, () => {
+  app.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 });
